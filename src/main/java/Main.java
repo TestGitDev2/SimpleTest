8ba24b5ca0075c9208e2528dbf00e98e8bd08acd
TestGitAdm <71552809+TestGitAdm@users.noreply.github.com>
2020-09-20 21:40:44 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("step 1 - adm");
-        System.out.printf("step 2 - adm");
-        System.out.println("step 4 - dev2 branch master");
-        System.out.println("step 5 - dev2 branch develop");
-        System.out.println("step 6 - dev2 branch real develop");
-        System.out.println("step 7 - dev2 branch develop");
-        System.out.println("step 8 - dev2 branch master");
-        System.out.println("step 9 - dev2 branch master");
-        System.out.println("step 10 - dev2 branch master");
-        System.out.println("step 11 - dev2 branch master");
     }
 }
