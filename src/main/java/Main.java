b84ed448884fe954e43b12cd419b36d9acd9c40f
TestGitDev2 <71592068+TestGitDev2@users.noreply.github.com>
2020-09-21 00:06:40 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("step 1 - adm");
         System.out.printf("step 3 - develop");
+        System.out.printf("step 4 - web");
         System.out.printf("step 5 - adm");
     }
 }
