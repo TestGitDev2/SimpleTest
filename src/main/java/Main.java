90288f4a398b380cb6bad2f6fcdf00e8415e36e6
VladimirKonstantinovA@gmail.com <89647165797@mail.ru>
2020-09-20 18:05:05 +0300

    Dev 2 commit 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.printf("step 2 - adm");
         System.out.println("step 4 - dev2 branch master");
         System.out.println("step 5 - dev2 branch develop");
+        System.out.println("step 6 - dev2 branch real develop");
     }
 }
