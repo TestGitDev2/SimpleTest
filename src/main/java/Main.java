874997399282669dbec5bb1633d41218413ce608
TestGitDev2 <71592068+TestGitDev2@users.noreply.github.com>
2020-09-20 22:57:06 +0300

    deleted str №2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("step 1 - adm");
-        System.out.printf("step 2 - adm");
         System.out.printf("step 3 - develop");
     }
 }
