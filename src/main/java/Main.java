ba3fd8b4fc415aea88b0a19be96d17a4e8622de4
VladimirKonstantinovA@gmail.com <89647165797@mail.ru>
2020-09-22 14:58:25 +0300

    commit at master!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.printf("step 1 - adm");
         System.out.printf("step 3 - adm");
         System.out.printf("step 4 - adm");
+        System.out.printf("step 4 - adm");
     }
 }
