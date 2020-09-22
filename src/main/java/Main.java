453ed7f9f0d7729373d38a4832502c8f660047c3
VladimirKonstantinovA@gmail.com <89647165797@mail.ru>
2020-09-22 14:47:50 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("step 1 - adm");
-        System.out.printf("step 3 - develop");
-        System.out.printf("step 4 - web");
-        System.out.printf("step 5 - adm");
-        System.out.printf("step 7 - develop");
-        System.out.printf("step 8");
-        System.out.printf("step 9");
-        System.out.printf("step 101");
-        System.out.printf("step 11 - dev");
-        System.out.printf("step 11 - github");
     }
 }
