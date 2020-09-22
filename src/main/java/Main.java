6f6d1b6ea4d0daf2238837e1068892951f49b27e
VladimirKonstantinovA@gmail.com <89647165797@mail.ru>
2020-09-22 15:05:18 +0300

    c(d)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("step 1 - develop");
         System.out.printf("step 2 - develop");
+        System.out.printf("step 3 - develop");
     }
 }
