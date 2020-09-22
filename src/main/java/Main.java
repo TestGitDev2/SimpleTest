aaadb08e2918675f6350498fb9186bdded6cd064
VladimirKonstantinovA@gmail.com <89647165797@mail.ru>
2020-09-22 18:45:58 +0300

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("step 1 - develop");
-        System.out.printf("step 2");
-        System.out.printf("step 3");
-        System.out.printf("step 4");
-        System.out.printf("step 5");
     }
 }
 л
