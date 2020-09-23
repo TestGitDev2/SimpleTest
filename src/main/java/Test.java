78c93f1a6b18036ddaafc691979dd2443816f582
VladimirKonstantinovA@gmail.com <89647165797@mail.ru>
2020-09-23 08:00:46 +0300

    d

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,5 @@ public class Test {
     public void get() {
         System.out.println("test 1");
         System.out.println("test 2");
-        System.out.println("test 3");
-        System.out.println("test 4");
     }
 }
